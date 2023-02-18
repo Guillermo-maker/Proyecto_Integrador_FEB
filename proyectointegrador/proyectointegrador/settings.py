d3d1c74dfe870570e9cf5112d26473ebf82e66ec
Guillermo-maker <gustavoandino2@gmail.com>
2023-02-18 18:36:01 -0300

    App instaladas andino_turismo, jazzmin

--- proyectointegrador/proyectointegrador/settings.py
+++ proyectointegrador/proyectointegrador/settings.py
@@ -31,13 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "andino_turismo"
 ]
+
+JAZZMIN_SETTINGS = {
+    "site_logo": "",
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
