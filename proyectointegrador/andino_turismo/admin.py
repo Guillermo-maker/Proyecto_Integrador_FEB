e6c087a4cea0fba604df3db812b8b4631a5b5d3d
Guillermo-maker <gustavoandino2@gmail.com>
2023-02-19 16:21:12 -0300

    Admin changes

--- proyectointegrador/andino_turismo/admin.py
+++ proyectointegrador/andino_turismo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Bus, Chofer,Atractivo, Parada, DetalleCadaParada, Recorrido, Viaje
+
+
 # Register your models here.
+
+admin.site.register(Bus)
+admin.site.register(Chofer)
+admin.site.register(Atractivo)
+admin.site.register(Parada)
+admin.site.register(DetalleCadaParada)
+admin.site.register(Recorrido)
+admin.site.register(Viaje)
